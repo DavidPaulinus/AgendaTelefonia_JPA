7eb95ee8bbb02cf380e10e68042ab9e96ff0fffb
DavidPaulinus <davipm@hotmail.com>
2023-01-26 09:27:37 -0300

    Projeto criado e configurado

---
+++ src/main/java/br/com/agenda/DAO/ContatoDAO.java
@@ -0,0 +1,5 @@
+package br.com.agenda.DAO;
+
+public class ContatoDAO {
+
+}
---
+++ src/main/java/br/com/agenda/application/Application.java
@@ -0,0 +1,9 @@
+package br.com.agenda.application;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
